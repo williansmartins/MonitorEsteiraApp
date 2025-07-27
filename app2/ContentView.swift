4aaa35717dfff14bae1f8cf15734d5433555234b
Willians Martins <professorwillians@gmail.com>
2025-07-26 22:15:36 -0700

    Initial Commit

---
+++ app2/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  app2
+//
+//  Created by User on 7/26/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ app2/app2App.swift
@@ -0,0 +1,17 @@
+//
+//  app2App.swift
+//  app2
+//
+//  Created by User on 7/26/25.
+//
+
+import SwiftUI
+
+@main
+struct app2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
